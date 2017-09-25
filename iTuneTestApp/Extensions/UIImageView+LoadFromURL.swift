4b14f97d15b1d895c38bfa24f050f69b9dd577cd
Tanay Roy <tanay.roy06@gmail.com>
2017-09-25 08:34:23 -0400

    initial commit

---
+++ iTuneTestApp/Extensions/UIImageView+LoadFromURL.swift
@@ -0,0 +1,31 @@
+//
+//  UIImageView+LoadFromURL.swift
+//  iTuneTestApp
+//
+//  Created by Tanay Kumar on 9/21/17.
+//  Copyright © 2017 Tanay Kumar. All rights reserved.
+//
+
+import UIKit
+
+
+/*This extension moves the image loading process of the UIImageView from the main thread to background thread to avoid extensive work on the main thread*/
+extension UIImageView {
+    
+    func loadImageUsingUrlString(_ urlString: String) {
+        let url = URL(string: urlString)
+        URLSession.shared.dataTask(with: url!, completionHandler: {
+            (data, respones, error) in
+            if error != nil {
+                print(error!)
+                return
+            }
+            DispatchQueue.main.async(execute: {
+                self.image = UIImage(data: data!)
+            })
+            
+        }).resume()
+    }
+    
+}
+
---
+++ iTuneTestApp/Model/SearchItem.swift
@@ -0,0 +1,21 @@
+//
+//  SearchItem.swift
+//  iTuneTestApp
+//
+//  Created by Tanay Kumar on 9/21/17.
+//  Copyright © 2017 Tanay Kumar. All rights reserved.
+//
+
+import UIKit
+
+struct SearchItem: Decodable {
+    
+    let trackName : String?
+    let artworkUrl60 : String?
+    let kind : String?
+    let trackPrice : Double?
+    let artworkUrl100 : String?
+    let description : String?
+    
+}
+
